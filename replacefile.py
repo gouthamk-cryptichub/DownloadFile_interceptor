433e54c7d4f22a8c4c5e267d109fa06b2cd43734
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-28 10:07:59 -0400

    packets filtered and ready to modify 1

--- replacefile.py
+++ replacefile.py
@@ -2,17 +2,19 @@
 import netfilterqueue as netq
 import scapy.all as scapy
 
+req_ack = []
 def mod_packet(packet):
     use_packet = scapy.IP(packet.get_payload())
     if use_packet.haslayer(scapy.Raw):
         if use_packet[scapy.TCP].dport == 80:
-            print("[+] HTTP Request...##########")
             if ".exe" in use_packet[scapy.Raw].load:
                 print("[+] Detected Download .exe file REQUEST...")
+                req_ack.append(use_packet[scapy.TCP].ack)
                 print(use_packet.show())
         elif use_packet[scapy.TCP].sport == 80:
-            print("[+] HTTP Response...#########")
-            print(use_packet.show())
+            if use_packet[scapy.TCP].seq in req_ack:
+                print("[+] HTTP Response for the Download Req...#########")
+                print(use_packet.show())
     packet.accept()
 
 queue = netq.NetfilterQueue()
